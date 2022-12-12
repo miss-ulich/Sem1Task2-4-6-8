a6231edf8c6ac9d71c5581c6183926746e826950
Olesia <miss-ulich@yandex.ru>
2022-12-12 22:28:47 +1000

    add Sem2 Task 19, 21, 23

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,46 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+//Ввод данных
+int number = ReadData("Введите пятизначное число: ");
+if ( number >= 10000 && number <= 99999)
+{
+
+    //Проверяем, является ли введенное число палиндромом
+    bool check = PalinTest(number);
+    if (check)
+    {
+        PrintData(number, "палиндром");
+    }
+    else
+    {
+        PrintData(number, " не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
+//Выводим данные на консоль
+int ReadData(string mesage)
+{
+        Console.Write(mesage);
+        return int.Parse(Console.ReadLine()??"0");
+}
+//Метод вывода данных
+void PrintData(int num, string msg)
+{
+    Console.WriteLine(num + msg);
+}
+bool PalinTest(int number)   
+{
+
+    bool result = false;
+    if ((number/1000)*100 + (number%100))
+    {
+    result = true;
+    } 
+    return result;
+}
---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,40 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+ int X1 = ReadData("Введите координату Х1: ");
+ int Y1 = ReadData("Введите координату Y1: ");
+ int Z1 = ReadData("Введите координату Z1: ");
+ int X2 = ReadData("Введите координату Х2: ");
+ int Y2 = ReadData("Введите координату Y2: ");
+ int Z2 = ReadData("Введите координату Z2: ");
+
+ double result = DistanceBetweenPoints(X1, Y1, Z1, X2, Y2, Z2);
+
+ PrintData("Расстояние между точками: ", result);
+
+
+ //PrintData("Расстояние между точками: ", DistanceBetweenPoints(ReadData("Введите координату Х1: "), ReadData("Введите координату Х1: "), ReadData("Введите координату Х1: "), ReadData("Введите координату Х1: ")));
+
+
+ // Метод читает данные от пользователя
+ int ReadData(string msg)
+ {
+     Console.WriteLine(msg);
+     return int.Parse(Console.ReadLine() ?? "0");
+ }
+
+ //Метод находит расстояние между точками на плоскости
+ double DistanceBetweenPoints(double X1, double Y1, double Z1, double X2, double Y2, double Z2)
+ {
+     double distance = Math.Sqrt((X1-X2)*(X1-X2)+(Y1-Y2)*(Y1-Y2)+(Z1-Z2)*(Z1-Z2));
+     return distance;
+ } 
+
+ //Метод выводит данные пользователю
+ void PrintData(string msg, double res)
+ {
+     Console.WriteLine(msg + res);
+}
+
+
