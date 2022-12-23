686aab15ed914d951d6051c294f9447300976212
Olesia <miss-ulich@yandex.ru>
2022-12-23 10:34:24 +1000

    add solution for Sem7 Task 47, 50, 52

---
+++ Sem7Task47/Program.cs
@@ -0,0 +1,63 @@
+﻿////  Задайте двумерный массив размером m×n, заполненный 
+//  случайными вещественными числами.     m = 3, n = 4.
+//   0,5 7 -2 -0,2
+//   1 -3,3 8 -9,9
+//   8 7,8 -7,1 9
+
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+   // Выводим сообщение
+    Console.Write(line);
+   // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+   // Возвращаем значение
+    return number;
+}
+
+// Метод, печатает двумерный массив вещественных чисел
+void Print2DArrayDouble(double[,] arr)
+{
+   // В циклах проходим по массиву, печатаем в консоль элементы, разделяя абзацем
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1) - 1; j++)
+        { 
+            Console.Write($"{Math.Round(arr[i, j], 2)}; ");
+        }
+
+    Console.WriteLine($"{Math.Round(arr[i, arr.GetLength(1) - 1], 2)}");
+    }
+}
+
+// Метод генерации и заполнения массива вещественных чисел
+double[,] Fill2DArrayDouble(int rows, int cols, double numMin, double numMax)
+{
+  // Создаём массив 
+    double[,] array2D = new double[rows, cols];
+
+  // Тест границ
+    if (numMin < numMax)
+    {
+     // Заполняем массив
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+               array2D[i, j] = new Random().NextDouble() * (numMax - numMin) + numMin;
+            }
+            }
+    }
+    return array2D;
+ }
+
+ // Данные для генерации массива
+double numMin = 10;
+double numMax = 99;
+
+int inputRows = ReadData("Введите количество строк: ");
+int inputCols = ReadData("Введите количество столбцов: ");
+
+ 
+Console.WriteLine($"Случайный массив вещественных чисел {inputRows}x{inputCols}:");
+Print2DArrayDouble(Fill2DArrayDouble(inputRows, inputCols, numMin, numMax));
---
+++ Sem7Task50/Program.cs
@@ -0,0 +1,91 @@
+﻿// Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает значение этого 
+// элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+//------------------------------------------------------------
+
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Метод, печатает двумерный массив
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод генерации и заполнения массива
+int[,] Fill2DArray(int rows, int cols, int numMin, int numMax)
+{
+    // Создаём массив
+    int[,] array2D = new int[rows, cols];
+
+    if (numMin < numMax)
+    {
+        // Заполняем массив
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                array2D[i, j] = new Random().Next(numMin, numMax + 1);
+            }
+        }
+    }
+    return array2D;
+}
+
+// Метод, принимает массив и позицию, печатает элемент в консоль
+void PrintArrayElement(int[,] arr, int row, int col)
+{
+    // Проверяем вхождение позиции элемента в массив 
+    bool ifArrayHasPosition = row > 0 && row <= arr.GetLength(0) &&
+                              col > 0 && col <= arr.GetLength(1);
+
+    if (ifArrayHasPosition)
+    {
+        PrintResult($"Элемент на позиции ({row},{col}): {arr[row - 1, col - 1]}");
+    }
+    else
+    {
+        PrintResult($"Указанная позиция за пределами массива");
+    }
+}
+
+// Данные для генерации массива
+int numMin = 10;
+int numMax = 99;
+
+int inputRows = ReadData("Введите количество строк: ");
+int inputCols = ReadData("Введите количество столбцов: ");
+int[,] array2D = Fill2DArray(inputRows, inputCols, numMin, numMax);
+PrintResult($"Случайный массив целых чисел {inputRows}x{inputCols}:");
+Print2DArray(array2D);
+Console.WriteLine();
+
+int rowPosition = ReadData("Введите номер строки: ");
+int colPosition = ReadData("Введите номер столбца: ");
+PrintArrayElement(array2D, rowPosition, colPosition);
