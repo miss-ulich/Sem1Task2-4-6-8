fc55e3b43c4bd8248b68dac47be7d5b88ecf3160
Olesia <miss-ulich@yandex.ru>
2022-12-31 17:27:32 +1000

    add solution for Sem 9 Task 64, 66, 68

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,39 @@
+﻿//----------------Задача 64-----------------
+// Напишите программу, которая выдаёт все 
+// натуральные числа в промежутке от N до 1
+//------------------------------------------
+
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+// Метод, число N, возвращает строку чисел от N до 1
+string LineGenRec(int num)
+{
+    if (num <= 0)
+    {
+        return string.Empty;
+    }
+    else
+    {
+        string outLine = num + " " + LineGenRec(num - 1);
+        return outLine;
+    }
+}
+
+int number = ReadData("Введите число N: ");
+string resultLine = LineGenRec(number);
+PrintResult(resultLine);
